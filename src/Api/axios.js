0f2824521eebb8b7dabed814d5483691b768940a
Gizachew Bewket <gizachewbewket19@gmail.com>
2025-09-22 17:45:13 +0300

    final commit1

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-const axiosInstance=axios.create({
-    baseURL:"http://localhost:5000",
+const axiosInstance = axios.create({
+  // baseURL:"http://localhost:5000",
+  //deployed version on render.com
+  baseURL: "https://amazone-api-deploy-uj8n.onrender.com",
+});
 
-})
-
-
-
-
-export  {axiosInstance};
+export { axiosInstance };
